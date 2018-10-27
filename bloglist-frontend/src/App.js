08cce19b57ee8783db66030cfba1a565a5daa932
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-10-27 13:43:56 +0300

    Implemented simple login
    Simple login implementation and conditional rendering

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       blogs: [],
+      error: null,
       username: '',
       password: '',
       user: null
@@ -18,48 +21,81 @@ class App extends React.Component {
       this.setState({ blogs })
     )
   } 
-  login = (event) => {
+  login = async (event) => {
     event.preventDefault()
-    console.log('logging in with', this.state.username, this.state.password)
+    console.log('login event thrown')
+    try {
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+      console.log(`Login service returned ${user}`)
+      this.setState({ username: '', password: '', user})
+    }
+    catch(exception) {
+      console.log('login failed')
+      this.setState({
+        error: 'invalid username or password',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
+    }
   }
   handleLoginFieldChange = (event) => {
     this.setState({ [event.target.name]: event.target.value })
   }
-
+  
   render() {
-    if (this.state.user === null) {
-    return (
+    const loginForm = () => (
       <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={this.login}>
-           <div>
-             username
-             <input
-               type="text"
-               name="username"
-               value={this.state.username}
-               onChange={this.handleLoginFieldChange}
-             />
-           </div>
-           <div>
-             password
-             <input
-               type="password"
-               name="password"
-               value={this.state.password}
-               onChange={this.handleLoginFieldChange}
-             />
-           </div>
-           <button type="submit">login</button>
-         </form>
-      </div>
-    )}
-    return (
+          <h2>Log in to application</h2>
+          <form onSubmit={this.login}>
+             <div>
+               username
+               <input
+                 type="text"
+                 name="username"
+                 value={this.state.username}
+                 onChange={this.handleLoginFieldChange}
+               />
+             </div>
+             <div>
+               password
+               <input
+                 type="password"
+                 name="password"
+                 value={this.state.password}
+                 onChange={this.handleLoginFieldChange}
+               />
+             </div>
+             <button type="submit">login</button>
+           </form>
+        </div>
+    )
+
+    const blogsForm = () => (
       <div>
          <h2>blogs</h2>
         {this.state.blogs.map(blog => 
           <Blog key={blog._id} blog={blog}/>
         )}
+      </div>
+    )
+    
+    if (this.state.user === null) {
+      return (
+        <div>
+          <Notification message={this.state.error} />
+          { loginForm() }
+        </div>
+      )
+    }
+    return (
+      <div>
+        <Notification message={this.state.error} />
+        <p>{this.state.user.name} logged in</p>
+        { blogsForm() }
       </div>
     )
   }
---
+++ bloglist-frontend/src/components/Notification.js
@@ -0,0 +1,5 @@
+import React from 'react'
+const Notification = ({message}) => (
+  <div>{message}</div>  
+)
+export default Notification
