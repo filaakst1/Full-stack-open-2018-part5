81556723c7140d43b967eafb90f5d2a84699ba13
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-10-27 12:29:51 +0300

    Login screen visualization
    Added rendered components for login screen

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -6,7 +6,10 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      username: '',
+      password: '',
+      user: null
     }
   }
 
@@ -17,14 +20,39 @@ class App extends React.Component {
   } 
 
   render() {
+    if (this.state.user === null) {
     return (
       <div>
-        <h2>blogs</h2>
+        <h2>Log in to application</h2>
+        <form onSubmit={this.login}>
+           <div>
+             username
+             <input
+               type="text"
+               value={this.state.username}
+               onChange={this.handleUsernameChange}
+             />
+           </div>
+           <div>
+             password
+             <input
+               type="password"
+               value={this.state.password}
+               onChange={this.handlePasswordChange}
+             />
+           </div>
+           <button type="submit">login</button>
+         </form>
+      </div>
+    )}
+    return (
+      <div>
+         <h2>blogs</h2>
         {this.state.blogs.map(blog => 
           <Blog key={blog._id} blog={blog}/>
         )}
       </div>
-    );
+    )
   }
 }
 
