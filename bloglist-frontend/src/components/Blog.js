a18ee80a47ab587a98baa6d07a05780963812e73
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-10-29 21:31:42 +0200

    Made blogs togglable
    Made blog togglable by click of the title of blog. Not sure if it was
    meant that the whole "title" div was clickable (title + author) or just
    the title of blog. Either way the implemention is somewhat similar in
    both cases

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,18 +1,39 @@
 import React from 'react'
-
-const Blog = ({blog}) => {
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5
+class Blog extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: false
+    }
   }
-  return (
-    <div style={blogStyle}>
-      {blog.title} {blog.author}
-    </div> 
-  )
+  toggleVisibility = () => {
+    this.setState({ visible: !this.state.visible })
+  }
+  render() {
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+    const showWhenVisible = { display: this.state.visible ? '' : 'none' }
+    return (
+      <div>
+        <div style={blogStyle} onClick={this.toggleVisibility} >
+          {this.props.blog.title} {this.props.blog.author}
+        </div>
+        <div style={showWhenVisible}>
+          <div style={blogStyle}>
+            <div><a href={this.props.blog.url} >{this.props.blog.url}</a></div>
+            <div>{this.props.blog.likes} likes <button>likes</button></div>
+            <div>added by {this.props.blog.user.name}</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Blog
