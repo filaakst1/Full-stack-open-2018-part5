0e60f86555a7933bdf88cff91f4bacb028ca302c
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-11-01 19:17:35 +0200

    Added PropTypes for BlogForm

--- bloglist-frontend/src/components/BlogForm.js
+++ bloglist-frontend/src/components/BlogForm.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 const BlogForm = ({handleSubmit, handleChange, title, author, url}) => {
-return (
-<div>
-  <h2>create new</h2>
-  <form onSubmit={handleSubmit} >
-    <div>title<input type="text" name="title" value={title} onChange={handleChange} /></div>
-    <div>author<input type="text" name="author" value={author} onChange={handleChange}/></div>
-    <div>url<input type="text" name="url" value={url} onChange={handleChange}/></div>
-    <div><button type="submit">create</button></div>
-  </form>
-</div>
-)
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleSubmit} >
+        <div>title<input type="text" name="title" value={title} onChange={handleChange} /></div>
+        <div>author<input type="text" name="author" value={author} onChange={handleChange}/></div>
+        <div>url<input type="text" name="url" value={url} onChange={handleChange}/></div>
+        <div><button type="submit">create</button></div>
+      </form>
+    </div>
+  )
+}
+BlogForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  title: PropTypes.func.isRequired,
+  author: PropTypes.func.isRequired,
+  url: PropTypes.func.isRequired
+  
 }
 export default BlogForm
