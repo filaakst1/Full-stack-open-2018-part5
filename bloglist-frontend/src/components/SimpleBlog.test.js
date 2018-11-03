c0ee6849aefbe54be07eb3062029b6c8b5beeaf6
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-11-03 15:45:18 +0200

    Tests for title and likes

--- bloglist-frontend/src/components/SimpleBlog.test.js
+++ bloglist-frontend/src/components/SimpleBlog.test.js
@@ -4,7 +4,7 @@ import SimpleBlog from './SimpleBlog'
 
 
 describe.only('<SimpleBlog />', () => {
-  it('renders content', () => {
+  it('renders title', () => {
     const blog = {
       title: 'Komponenttitestaus tapahtuu jestillä ja enzymellä',
       author: 'filaakst',
@@ -13,8 +13,29 @@ describe.only('<SimpleBlog />', () => {
     const onClick = () => {}
 
     const simpleBlogComponent = shallow(<SimpleBlog blog={blog} onClick={onClick} />)
-    const contentDiv = simpleBlogComponent.find('.content')
+    console.log(simpleBlogComponent.debug())
 
-    expect(contentDiv.text()).toContain(note.content)
+    const titleDiv = simpleBlogComponent.find('.title')
+    console.log(titleDiv.debug())
+
+    expect(titleDiv.text()).toContain(`${blog.title} ${blog.author}`)
+
+  })
+  it('renders likes', () => {
+    const blog = {
+      title: 'Komponenttitestaus tapahtuu jestillä ja enzymellä',
+      author: 'filaakst',
+      likes: 10
+    }
+    const onClick = () => {}
+
+    const simpleBlogComponent = shallow(<SimpleBlog blog={blog} onClick={onClick} />)
+    console.log(simpleBlogComponent.debug())
+
+    const titleDiv = simpleBlogComponent.find('.likes')
+    console.log(titleDiv.debug())
+
+    expect(titleDiv.text()).toContain(`blog has ${blog.likes} likes`)
+
   })
 })
