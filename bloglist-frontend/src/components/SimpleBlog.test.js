39429b8525144316487629eabbb048084b32a1c4
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-11-03 15:46:30 +0200

    Merged into single single test

--- bloglist-frontend/src/components/SimpleBlog.test.js
+++ bloglist-frontend/src/components/SimpleBlog.test.js
@@ -4,7 +4,7 @@ import SimpleBlog from './SimpleBlog'
 
 
 describe.only('<SimpleBlog />', () => {
-  it('renders title', () => {
+  it('renders simple blog', () => {
     const blog = {
       title: 'Komponenttitestaus tapahtuu jestillä ja enzymellä',
       author: 'filaakst',
@@ -17,25 +17,13 @@ describe.only('<SimpleBlog />', () => {
 
     const titleDiv = simpleBlogComponent.find('.title')
     console.log(titleDiv.debug())
-
     expect(titleDiv.text()).toContain(`${blog.title} ${blog.author}`)
 
-  })
-  it('renders likes', () => {
-    const blog = {
-      title: 'Komponenttitestaus tapahtuu jestillä ja enzymellä',
-      author: 'filaakst',
-      likes: 10
-    }
-    const onClick = () => {}
+    const likeDiv = simpleBlogComponent.find('.likes')
+    console.log(likeDiv.debug())
 
-    const simpleBlogComponent = shallow(<SimpleBlog blog={blog} onClick={onClick} />)
-    console.log(simpleBlogComponent.debug())
-
-    const titleDiv = simpleBlogComponent.find('.likes')
-    console.log(titleDiv.debug())
-
-    expect(titleDiv.text()).toContain(`blog has ${blog.likes} likes`)
+    expect(likeDiv.text()).toContain(`blog has ${blog.likes} likes`)
 
   })
+
 })
