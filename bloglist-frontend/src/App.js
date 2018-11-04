0226664484c1d23ce5dbd332d46da1d48d44f999
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-11-04 12:49:37 +0200

    Added tests
    Added tests for rendering blogs and blog form while user is logged in.

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -38,7 +38,7 @@ class App extends React.Component {
       this.setState({ user })
       blogService.setToken(user.token)
     }
-  } 
+  }
   /**
    * Logout function
    */
--- bloglist-frontend/src/App.test.js
+++ bloglist-frontend/src/App.test.js
@@ -9,23 +9,56 @@ jest.mock('./services/blogs')
 
 describe('<App />', () => {
   let app
-  beforeAll(() => {
-    app = mount(<App />)
-  })
   describe('user not logged in', () => {
     beforeEach(() => {
-      // luo sovellus siten, että käyttäjä ei ole kirjautuneena
+      app = mount(<App />)
     })
-    it('renders only login', () => {
-      app.update()
+    it('login form not rendered', () => {
       const loginForm = app.find(LoginForm)
-      console.log(loginForm.debug())
+      expect(loginForm).toHaveLength(1)
       expect(app.html()).toContain(loginForm.html())
+    })
+    it('blog form is not rendered', () => {
+      app.update()
       const blogForm = app.find(BlogForm)
       // Wrapper did not find anything
       expect(blogForm).toHaveLength(0)
+    })
+    it('blogs are not rendered', () => {
+      app.update()
       const blogs = app.find(Blog)
+      // Wrapper did not find anything
       expect(blogs).toHaveLength(0)
+    })
+  })
+
+  describe('user logged in', () => {
+    beforeEach(() => {
+      const user = {
+        username: 'tester',
+        token: '1231231214',
+        name: 'Teuvo Testaaja'
+      }
+      localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      app = mount(<App />)
+    })
+    it('login form not rendered', () => {
+      app.update()
+      const loginForm = app.find(LoginForm)
+      // login form not found
+      expect(loginForm).toHaveLength(0)
+    })
+    it('blog form is rendered', () => {
+      app.update()
+      // Test that blog form is rendered
+      const blogForm = app.find(BlogForm)
+      expect(blogForm).toHaveLength(1)
+    })
+    it('blogs are rendered', () => {
+      app.update()
+      // Test that 3 blogs are rendered
+      const blogs = app.find(Blog)
+      expect(blogs).toHaveLength(3)
     })
   })
 })
