de342944f033471b2ed9dabaf5b234e37edcd283
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-11-04 12:25:36 +0200

    Fixed test to find Blog components too
    Test was previously finding BlogForms twice. Replaced latter find to
    search for Blogs

--- bloglist-frontend/src/App.test.js
+++ bloglist-frontend/src/App.test.js
@@ -12,17 +12,20 @@ describe('<App />', () => {
   beforeAll(() => {
     app = mount(<App />)
   })
-  it('renders only login', () => {
-    app.update()
-    const loginForm = app.find(LoginForm)
-    console.log(loginForm.debug())
-    expect(app.html()).toContain(loginForm.html())
-    const blogForm = app.find(BlogForm)
-    // Wrapper did not find anything
-    expect(blogForm).toHaveLength(0)
-    const blogs = app.find(BlogForm)
-    expect(blogs).toHaveLength(0)
+  describe('user not logged in', () => {
+    beforeEach(() => {
+      // luo sovellus siten, että käyttäjä ei ole kirjautuneena
+    })
+    it('renders only login', () => {
+      app.update()
+      const loginForm = app.find(LoginForm)
+      console.log(loginForm.debug())
+      expect(app.html()).toContain(loginForm.html())
+      const blogForm = app.find(BlogForm)
+      // Wrapper did not find anything
+      expect(blogForm).toHaveLength(0)
+      const blogs = app.find(Blog)
+      expect(blogs).toHaveLength(0)
+    })
   })
-
-
 })
