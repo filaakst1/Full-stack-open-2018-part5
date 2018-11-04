47a6c330551328f7e568164241c27da170d822c7
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-11-04 15:24:20 +0200

    Moved Statistiikka to own component

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,50 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Statistiikka = () => {
-  const palautteita = 0
-
-  if (palautteita === 0) {
-    return (
-      <div>
-        <h2>stataistiikka</h2>
-        <div>ei yhtään palautetta annettu</div>
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <h2>statistiikka</h2>
-      <table>
-        <tbody>
-          <tr>
-            <td>hyvä</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>neutraali</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>huono</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>keskiarvo</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>positiivisia</td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-
-      <button>nollaa tilasto</button>
-    </div >
-  )
-}
+import Statistiikka from './components/Statistiikka'
 
 class App extends React.Component {
   klik = (nappi) => () => {
